06cc63378a37d257e73aedfd4b0578e5035b3f89
ntt97 <nthanhtodev97@gmail.com>
2020-12-29 22:45:19 +0700

    add eslin

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
+  printWidth: 120,
   trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  useTabs: false,
+  jsxSingleQuote: false,
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.png',
+        ],
+        alias: {
+          '@store': './src/store',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@actions': './src/actions',
+          '@constants': './src/constants',
+          '@reducers': './src/reducers',
+          '@epics': './src/epics',
+          '@utils': './src/utils',
+          '@sagas': './src/sagas',
+          '@global': './src/global',
+        },
+      },
+    ],
+  ],
 };
